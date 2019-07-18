ade1252ce12f6d7445915635283293f986ac424e
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 18:25:49 +0000

    Automatically backed up by Learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,22 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state, action){
+  switch (action.type) { 
+      case 'ADD_PET':
+        return { count: state.count + 1 }
+      case 'REMOVE_PET':
+        return { count: state.count + 1 }
+      default:
+        return state;
+  }
 }
 
 export function dispatch(){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
-
+  document.getElementById('container').innerHTML = '<ul>' + state.pets.map(pet => `<li>${pet.name}</li>`) + '</ul>'; 
 }
