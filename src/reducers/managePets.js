88ae61755efd7cfd46df7f2a04d0feed176590cb
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 18:31:44 +0000

    Automatically backed up by Learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -20,4 +20,4 @@ export function render(){
   document.getElementById('container').innerHTML = '<ul>' + state.pets.map(pet => `<li>${pet.name}</li>`) + '</ul>';
 }
 
-dispatch({type: '@@INIT'});
+// dispatch({type: '@@INIT'});
